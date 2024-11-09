5a0fd3cc093dbbc22aad9308e88b7414afccb944
David Xia <davidxia143@gmail.com>
2024-11-09 13:03:44 -0600

    updated seasonal brier graphs

--- src/analysis/seasonal_brier.py
+++ src/analysis/seasonal_brier.py
@@ -4,7 +4,7 @@ from sklearn.metrics import brier_score_loss
 
 
 # calculates brier score for entirety of league by season
-league = "mlb"
+league = "nba"
 df = pd.read_csv(f'processed_data/{league}_espn_combined.csv')
 
 season_groups = df.groupby('tournament')
