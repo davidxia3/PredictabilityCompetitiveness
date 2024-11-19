fe7080dd85a7a33df3d68ac7387116af13c5ffa4
David Xia <davidxia143@gmail.com>
2024-11-18 23:57:49 -0600

    updated fivethirtyeight comparison

--- src/analysis/fivethirtyeight_comparison.py
+++ src/analysis/fivethirtyeight_comparison.py
@@ -5,25 +5,30 @@ df = pd.read_csv('processed_data/nfl_espn_combined.csv')
 
 seasons = ['NFL_2019', 'NFL_2020', 'NFL_2022', 'NFL_2023']
 
-# for season in seasons:
-#     score_avg = 0
-#     for index, row in df.iterrows():
-#         if row['tournament'] != season:
-#             continue
-#         if row['game_type'] != 'regular_season' and row['game_type'] != 'postseason':
-#             continue
+for season in seasons:
+    score_avg = 0
+    for index, row in df.iterrows():
+        s = 0
+        if row['tournament'] != season:
+            continue
+        if row['game_type'] != 'regular_season' and row['game_type'] != 'postseason':
+            continue
 
-#         prediction_avg = row['avg_prob_1']*100
-#         result = row['result']
+        prediction_avg = row['avg_prob_1']*100
+        result = row['result']
 
-#         if result == 1:
-#             score_avg += 25 - (((prediction_avg-100)**2)/100)
-#         else:
-#             score_avg += 25 - (((prediction_avg)**2)/100)
+        if result == 1:
+            s = 25 - (((prediction_avg-100)**2)/100)
+        else:
+            s = 25 - (((prediction_avg)**2)/100)
 
-#     print(score_avg)
+        if row['game_type'] == 'postseason':
+            s = s * 2
 
-placements = [39, 27, 70, 48]
+        score_avg += s
+    print(score_avg)
+
+placements = [30, 27, 53, 11]
 totals = [15073 + 1, 15141 + 1, 9781 + 1, 9983 + 1]
 percentiles = []
 for i in range(len(totals)):
