b01cd56c259e770581045f8d424e2c595a35ad6e
David Xia <davidxia143@gmail.com>
2024-11-18 22:52:01 -0600

    minor bug fixing

--- src/analysis/brier_by_team.py
+++ src/analysis/brier_by_team.py
@@ -1,36 +1,30 @@
 import pandas as pd
+import numpy as np
 
 league = 'nfl'
 
 df = pd.read_csv(f'processed_data/{league}_espn_combined.csv')
-
 df['pred_prob'] = df['avg_prob_1']
 
 brier_scores = {}
 
+df['brier_score'] = (df['pred_prob'] - df['result']) ** 2
+
 teams = pd.concat([df['team_1'], df['team_2']]).unique()
-tournaments = pd.concat([df['tournament']]).unique()
+tournaments = df['tournament'].unique()
 tournaments.sort()
 
 for team in teams:
-    team_data = []
-    for tournament in tournaments:
-        score = 0
-        count = 0
-        for index, row in df.iterrows():
-            if row['tournament'] == tournament:
-                if row['team_2'] == team or row['team_1'] == team:
-                    count = count + 1
-                    score = score + ((row['avg_prob_1'] - row['result'])**2)
+    team_data = (
+        df[(df['team_1'] == team) | (df['team_2'] == team)] 
+        .groupby('tournament')['brier_score']               
+        .mean()                                            
+        .reindex(tournaments, fill_value=np.nan)           
+    )
+    brier_scores[team] = team_data.values 
 
-        team_data.append(score/count)
-
-    brier_scores[team] = team_data
-
-brier_df = pd.DataFrame(brier_scores)
-
-brier_df = brier_df.reset_index()
-
+brier_df = pd.DataFrame(brier_scores, index=tournaments).reset_index()
+brier_df = brier_df.rename(columns={'index': 'tournament'}) 
 brier_df = brier_df.round(4)
-brier_df.to_csv(f'results/{league}_team_brier.csv', index=False)
 
+brier_df.to_csv(f'results/{league}_team_brier.csv', index=False)
--- src/analysis/fivethirtyeight_comparison.py
+++ src/analysis/fivethirtyeight_comparison.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('processed_data/nfl_espn_combined.csv')
