789a96cc4a98bc102c0139ef74a2c329fc3938a7
David Xia <davidxia143@gmail.com>
2024-11-27 13:13:12 -0600

    added uniform calibration

--- src/analysis/seasonal_calibration.py
+++ src/analysis/seasonal_calibration.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.calibration import calibration_curve
 
-league = 'nfl'
+league = 'mlb'
 
 df = pd.read_csv(f'processed_data/{league}_espn_combined.csv')
 
@@ -11,9 +11,9 @@ predicted_probs = df['avg_prob_1']
 true_results = df['result']
 
 fraction_of_positives, mean_predicted_probabilities = calibration_curve(
-    true_results, predicted_probs, n_bins=10, strategy='quantile'
+    true_results, predicted_probs, n_bins=10, strategy='uniform'
 )
 
 plt.plot(mean_predicted_probabilities, fraction_of_positives, 's-', label="calibration curve")
 plt.plot([0, 1], [0, 1], 'k--', label="perfect calibration")
-plt.savefig(f'results/{league}_calibration.png')
+plt.savefig(f'results/{league}_calibration_uniform.png')
