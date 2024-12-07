2e8f5afd8140f3cdc52fbc977a82631ab9a122a1
David Xia <davidxia143@gmail.com>
2024-12-07 13:27:33 -0600

    updated overall seasonal

--- src/graphing/overall_seasonal.py
+++ src/graphing/overall_seasonal.py
@@ -7,16 +7,27 @@ colors = ['blue', 'green', 'red', 'purple']
 
 plt.figure(figsize=(10, 6))
 
+all_years = set()
+
 for i, file_path in enumerate(file_paths):
     df = pd.read_csv(file_path)
     
     df['year'] = df['season'].str.extract(r'_(\d{4})').astype(int)
     
-    plt.plot(df['year'], df['brier_score'], color=colors[i], label=leagues[i])
+    df = df[(df['year'] >= 2009) & (df['year'] <= 2023)]
+    
+    all_years.update(df['year'])
+    
+    plt.plot(df['year'], df['brier_score'], color=colors[i], label=leagues[i], marker = 'o')
 
-plt.xlabel("Year")
+all_years = sorted(all_years)
+plt.xticks(ticks=all_years, labels=[str(year) for year in all_years], rotation=45)
+
+plt.ylim(0.15, 0.26)
+
+plt.xlabel("Season")
 plt.ylabel("Brier Score")
-plt.title("Seasonal Brier Scores by League")
+plt.title("Seasonal Brier Scores by League (2009–2023)")
 plt.legend()
 plt.grid(True)
 
