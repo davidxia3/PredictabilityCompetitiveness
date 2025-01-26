59a564bc4de9ff6ae5617c4027d2d1381afae865
David Xia <davidxia143@gmail.com>
2025-01-26 02:13:18 -0600

    binary analysis

---
+++ src/analysis/binary_analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+accuracies = []
+files = [
+    "processed_data/mlb_espn_combined.csv",
+    "processed_data/nba_espn_combined.csv",
+    "processed_data/nhl_espn_combined.csv",
+    "processed_data/nfl_espn_combined_with_elo.csv"
+]
+
+for file in files:
+    df = pd.read_csv(file)
+    df['predicted'] = (df['avg_prob_1'] >= 0.5).astype(int)
+    accuracy = (df['predicted'] == df['result']).mean()
+    accuracies.append(accuracy)
+
+df = pd.read_csv("processed_data/nfl_espn_combined_with_elo.csv")
+df['predicted'] = (df['elo_prob_1'] >= 0.5).astype(int)
+accuracy = (df['predicted'] == df['result']).mean()
+accuracies.append(accuracy)
+
+labels = ["MLB_ML", "NBA_ML", "NHL_ML", "NFL_ML", "NFL_ELO"]
+
+output_df = pd.DataFrame({'label': labels, 'accuracy': accuracies})
+output_df.to_csv("results/binary_accuracies.csv", index=False)
+
