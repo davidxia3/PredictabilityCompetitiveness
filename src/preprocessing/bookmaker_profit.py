17d357af4814eea6feaba47ba7fc9ba95f56eb7a
David Xia <davidxia143@gmail.com>
2025-11-15 04:32:57 -0600

    fixed bookmaker profit

--- src/preprocessing/bookmaker_profit.py
+++ src/preprocessing/bookmaker_profit.py
@@ -11,9 +11,20 @@ for league in leagues:
 
     df = pd.read_csv(file_path)
 
-    df['raw_prob_1'] = (abs(df['avg_moneyline_1']) / (abs(df['avg_moneyline_1']) + 100))
-    df['raw_prob_2'] = (abs(df['avg_moneyline_2']) / (abs(df['avg_moneyline_2']) + 100))
-    
+    for idx, row in df.iterrows():
+        team_1_prob = row["avg_prob_1"]
+        
+        ml1 = row["avg_moneyline_1"]
+        ml2 = row["avg_moneyline_2"]
+
+        if team_1_prob >= 0.5:
+            # team 1 is favorite
+            df.loc[idx, "raw_prob_1"] = abs(ml1) / (abs(ml1) + 100)
+            df.loc[idx, "raw_prob_2"] = 100 / (abs(ml2) + 100)
+        else:
+            # team 2 is favorite
+            df.loc[idx, "raw_prob_1"] = 100 / (abs(ml1) + 100)
+            df.loc[idx, "raw_prob_2"] = abs(ml2) / (abs(ml2) + 100)
 
     df['raw_prob_1'] = df['raw_prob_1'].apply(lambda x: math.trunc(x * 10000) / 10000)
     df['raw_prob_2'] = df['raw_prob_2'].apply(lambda x: math.trunc(x * 10000) / 10000)
